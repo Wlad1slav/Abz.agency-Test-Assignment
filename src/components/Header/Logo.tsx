1cbaeea0768c302c29048aa0edad47008042a34b
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-07-10 19:37:14 +0300

    Setted the height for the link in which the logo is located

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Logo() {
     return (
-        <Link href='/'>
+        <Link href='/' style={{height: "26px"}}>
             <Image type="thumbnail" width="104px" height="26" src="/logo.svg" alt="logo" />
         </Link>
     )
