a5b56d6905865cb1fad0ba860d3394c5a707ef78
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-07-11 16:23:48 +0300

    Fixed typo

--- src/components/Sections/PostRequestSection/PostRequestSection.tsx
+++ src/components/Sections/PostRequestSection/PostRequestSection.tsx
@@ -6,6 +6,11 @@ import {RadioButtonProps} from "@/types/props";
 import FormPost from "@/components/Form/FormPost/FormPost";
 import './PostRequestSection.scss';
 
+interface PositionResponse {
+    name: string;
+    id: string;
+}
+
 function PostRequestSection() {
     const [positions, setPositions] = useState<Array<RadioButtonProps>>()
 
@@ -13,7 +18,7 @@ function PostRequestSection() {
     useEffect(() => {
         axios.get('https://frontend-test-assignment-api.abz.agency/api/v1/positions')
             .then(r => {
-                const positionsResponse: Array<RadioButtonProps> = r.data.positions.map(position => {
+                const positionsResponse: Array<RadioButtonProps> = r.data.positions.map((position: PositionResponse) => {
                     return {label: position.name, value: position.id};
                 });
                 setPositions(positionsResponse);
